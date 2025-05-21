cfebe8e53b8a12114c721d9affbe901f5e4c18a5
sbrown555 <sbrown@berkeley.edu>
2025-05-20 18:30:51 -0700

    Update meteorology.py

--- meteorology.py
+++ meteorology.py
@@ -144,8 +144,10 @@ for name in dict_df.keys():
         st.write(i)
         copy = frame
         copy[interval_name] = copy[interval_name] + 25
-        copy.set_index(interval_name, inplace = True)
-        frame.set_index(interval_name, inplace = True)
+        st.write(f"copy column names  = {copy.columns}")
+        st.write(f"frame column names  = {frame.columns}")
+        # copy.set_index(interval_name, inplace = True)
+        # frame.set_index(interval_name, inplace = True)
         frame_test = len(list(set(frame[interval_name].to_list())))
         st.write(frame_test)
         copy_test = len(list(set(copy[interval_name].to_list())))
