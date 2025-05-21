de4b7207a8dc72cbea40380e93c6d9e3d9b5f2fe
sbrown555 <sbrown@berkeley.edu>
2025-05-20 23:04:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,8 +167,14 @@ if 'All' in filter_temp:
 
 filter_temp = [x for x in filter_temp if x != 'All']
 
-if ['HiC', 'LowC'] in filter_temp or ['Upper', 'Lower'] in filter_temp or ['Wet', 'Dry'] in filter_temp or ['QUCH', 'QUWI', 'PIPO', 'PISA'] in filter_temp:
-    st.warning("Filter warning: Don't choose all options from a particular field at once")
+list_contradictions = [['HiC', 'LowC'], ['Upper', 'Lower'], ['Wet', 'Dry'], ['QUCH', 'QUWI', 'PIPO', 'PISA']]
+for cont in list_contradictions:
+    if all(item in filter_temp for item in cont):
+        st.warning("Filter warning: Don't choose all options from a particular field at once")
+
+
+# if ['HiC', 'LowC'] in filter_temp or ['Upper', 'Lower'] in filter_temp or ['Wet', 'Dry'] in filter_temp or ['QUCH', 'QUWI', 'PIPO', 'PISA'] in filter_temp:
+#     st.warning("Filter warning: Don't choose all options from a particular field at once")
 
 for term in filter_temp:
     df_temp = df_temp[df_temp.isin([term]).any(axis=1)]
