3aa08e7853040bee8ed466b8cdfffd881c439289
sbrown555 <sbrown@berkeley.edu>
2025-05-20 19:54:48 -0700

    Update meteorology.py

--- meteorology.py
+++ meteorology.py
@@ -179,7 +179,7 @@ for name in dict_df.keys():
 # st.write(dict_redundant.keys())
 dict_diff = {}
 df_1.reset_index(inplace=True)
-df_1.rename(columns={'1.0_hour_interval':interval_name})
+df_1.rename(columns={'1.0_hour_interval': '2.0_hour_interval'})
 st.write(df_1.columns)
 df_1.set_index(['site', long_int_name, interval_name], inplace=True)
 st.write(df_1)
