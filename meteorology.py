a65d129f731fe9e760861bd322206e7e61b9b1e9
sbrown555 <sbrown@berkeley.edu>
2025-05-21 01:57:56 -0700

    Update meteorology.py

--- meteorology.py
+++ meteorology.py
@@ -257,6 +257,7 @@ for name in dict_diff.keys():
   # plt.tight_layout()
   # st.pyplot(fig)
   grouped_interval = dataframe.groupby(['site',interval_name]).agg({col:'max' for col in variables})
+  xaxis = interval_name
   for site, group in grouped_interval.groupby("site"):
     plt.plot(group[xaxis],group[yaxis], label = site)
   plt.title(yaxis)
