cd8be8b45e8c9818c711145d95d0e3d49a39a9b8
sbrown555 <sbrown@berkeley.edu>
2025-05-20 23:19:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,7 @@ df_temp['Species'] = split_cols[2]
 df_temp['Sensor_Position'] = split_cols[3]
 
 filter_options_temp = ['All sensors', 'Upper', 'Lower', 'HiC', 'LowC', 'Wet', 'Dry', 'QUCH', 'QUWI', 'PIPO', 'PISA']
-filter_temp = st.multiselect(label = "Filter sensors (select sensors whose names contain all of:", options = filter_options_temp, default = 'All sensors', key="temp_filter")
+filter_temp = st.multiselect(label = "Filter sensors (select sensors whose names contain all of):", options = filter_options_temp, default = 'All sensors', key="temp_filter")
 
 if 'All_sensors' in filter_temp:
     filter_temp = []
@@ -203,7 +203,7 @@ df_vwc['Species'] = split_cols[2]
 df_vwc['Sensor_Position'] = split_cols[3]     
 
 filter_options_vwc = ['All sensors', 'Upper', 'Lower', 'HiC', 'LowC', 'Wet', 'Dry', 'QUCH', 'QUWI', 'PIPO', 'PISA']
-filter_vwc = st.multiselect(label = "Filter sensors (select sensors whose names contain all of:", options = filter_options_vwc, default = 'All sensors', key="vwc_filter")
+filter_vwc = st.multiselect(label = "Filter sensors (select sensors whose names contain all of):", options = filter_options_vwc, default = 'All sensors', key="vwc_filter")
 
 if 'All sensors' in filter_vwc:
     filter_vwc = []
