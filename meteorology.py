ee7e0e447750666819b282be3b0c6326f268e12c
sbrown555 <sbrown@berkeley.edu>
2025-05-20 18:27:01 -0700

    Update meteorology.py

--- meteorology.py
+++ meteorology.py
@@ -138,6 +138,7 @@ for name in dict_df.keys():
       df_1 = frame
     elif interval_short > 1:
       num_copies = interval_short - 1
+      st.write(f"num_copies = {num_copies}")
       frame[interval_name] = frame[interval_name]*interval_short
       for i in range(num_copies):
         st.write(i)
